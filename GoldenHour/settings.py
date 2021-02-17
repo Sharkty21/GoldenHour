3c112135cf7f17f6e1b1cc12e77de03e261258d8
Sharkty21 <tylermsharkey@gmail.com>
2021-02-16 18:43:19 -0600

    npm installs

--- GoldenHour/settings.py
+++ GoldenHour/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weatherAPI.apps.WeatherapiConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
---
+++ weatherAPI/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+def getTemperature(zipCode):
+    return zipCode
